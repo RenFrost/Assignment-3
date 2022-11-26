18ab6dc12d5f78a4f3fb3227790c6698b275b0cb
RenFrost <diidnugroho@gmail.com>
2022-11-26 18:46:24 +0700

    7th

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -6,17 +6,9 @@ import List from '@mui/material/List'
 import Divider from '@mui/material/Divider'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
-import {mainNavbarItems} from '../consts/navbarItems'
+import { mainNavbarItems } from '../consts/navbarItems'
 import { navbarStyles } from './styles'
-import {useParams, useNavigate} from 'react-router-dom'
-
-{/* <Link to="/">Homepage</Link> |  
-            <Link to="/about">About Me</Link> |
-            <Link to="/experiences">Work Experience</Link> |
-            <Link to="/skills">Skills</Link> |
-            <Link to="/interests">Interests</Link> |
-            <Link to="/certificates">Certificates</Link> |
-            <Link to="/Awards">Awards</Link>  */}
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
   const navigate = useNavigate()
